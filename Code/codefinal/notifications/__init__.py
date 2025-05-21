673cba7d2cf24d06aa284c2f1b8333cb223545b3
Petros Maragkos <107312066+petros4ever@users.noreply.github.com>
2025-05-21 12:49:52 +0300

    Delete Code/codefinal/notifications directory

--- Code/codefinal/notifications/__init__.py
+++
@@ -1,3 +0,0 @@
-# notifications/__init__.py
-from .notifications import NotificationService, Notification   # re-export   <-- add this
-__all__ = ["NotificationService", "Notification"]             # optional
